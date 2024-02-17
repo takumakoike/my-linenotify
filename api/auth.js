e80febcd4c495b23c005a50d7adb3cbffe8f6890
takumakoike <takuma.151e@gmail.com>
2024-02-17 20:16:02 +0900

    なにか

--- api/auth.js
+++ api/auth.js
@@ -1,4 +1,3 @@
-// Vercelのサーバーレス関数でLINEのアクセストークンを取得
 export default async (req, res) => {
     if (req.method === 'POST') {
         const { code } = req.body;
@@ -8,31 +7,31 @@ export default async (req, res) => {
         const REDIRECT_URI = process.env.REDIRECT_URI;
 
         try {
-        const response = await fetch("https://notify-bot.line.me/oauth/token", {
-            method: "POST",
-            headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-            },
-            body: new URLSearchParams({
-                grant_type: "authorization_code",
-                code:code,
-                redirect_uri: REDIRECT_URI,
-                client_id: CLIENT_ID,
-                client_secret: CLIENT_SECRET,
-            })
-        });
+            const response = await fetch("https://notify-bot.line.me/oauth/token", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded",
+                },
+                body: new URLSearchParams({
+                    grant_type: "authorization_code",
+                    code:code,
+                    redirect_uri: REDIRECT_URI,
+                    client_id: CLIENT_ID,
+                    client_secret: CLIENT_SECRET,
+                })
+            });
 
-        const data = await response.json();
+            const data = await response.json();
 
-        if (data.access_token) {
-            // アクセストークンをクライアントに返す
-            res.status(200).json({ accessToken: data.access_token });
-        } else {
-            // エラーレスポンス
-            res.status(400).json({ error: "Failed to obtain access token" });
-        }
+            if (data.access_token) {
+                // アクセストークンをクライアントに返す
+                res.status(200).json({ accessToken: data.access_token });
+            } else {
+                // エラーレスポンス
+                res.status(400).json({ error: "Failed to obtain access token" });
+            }
         } catch (error) {
-        res.status(500).json({ error: error.message });
+            res.status(500).json({ error: error.message });
         }
     } else {
         // POSTリクエスト以外は許可しない
